e938dee2e1f7676de5a6c09ec02c30fcfab5ae11
Daniel BR <daniel@messoft.com>
2017-09-01 16:56:46 -0500

    Add iOS target

---
+++ Cart iOS/Cart iOS.h
@@ -0,0 +1,19 @@
+//
+//  Cart iOS.h
+//  Cart iOS
+//
+//  Created by Daniel on 9/1/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cart iOS.
+FOUNDATION_EXPORT double Cart_iOSVersionNumber;
+
+//! Project version string for Cart iOS.
+FOUNDATION_EXPORT const unsigned char Cart_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cart_iOS/PublicHeader.h>
+
+
